95ca7a63fca19de04c64c9fe089c73d3c0ea248d
Phil Wilkinson <phil@philwilky.me>
2023-05-10 21:26:19 +0100

    Starter Code

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,50 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using Newtonsoft.Json.Bson;
-using static UnityEditor.Experimental.GraphView.GraphView;
-using System.Reflection;
-using Unity.VisualScripting;
-using UnityEditor;
-using static UnityEngine.ParticleSystem;
-using System.Buffers.Text;
 using System;
+
 public class UiManager : MonoBehaviour
 {
 
-//GOAL:
-    //I am trying to make a simple tycoon type game based off of the first Goldsmith banker of the 1600s.I don't yet have a game loop
-    //but i have gold in a vault that can be used to issue promissory notes, i know the bankers of the time gave out more notes than they
-    //had gold bars in their vault. can you help me with ideas.
+    public TMP_Text txtGold;
+    public int goldInVault;
 
 
- //SOME IDEAS:
-    //Create a system for issuing promissory notes: In your game, players should be able to issue promissory notes using the gold in their vault.
-    //You could make it so that players have to pay interest on the notes they issue, and if they can't pay back the notes, their reputation
-    //with other bankers and merchants will suffer.
-
-    //Introduce risk and uncertainty: In the 1600s, the banking industry was inherently risky, and your game should reflect that.
-    //You could introduce random events that affect the value of gold or the demand for promissory notes, and players would need to adjust
-    //their strategies accordingly.
-
-    //Implement competition: The banking industry is competitive, and your game should reflect that too.
-    //You could introduce other bankers as AI opponents or have players compete against each other to see who can accumulate the most wealth and prestige.
-
-    //Introduce historical accuracy: Since your game is based on the Goldsmith banker of the 1600s,you could introduce some historical accuracy
-    //to the game. For example, players could face restrictions on the amount of gold they could hold, or they might face consequences
-    //if they issued too many promissory notes.
-
-    //Implement a feedback system: Players should receive feedback on their performance in the game, such as how much interest they're earning or
-    //how their reputation is affected by their actions. This feedback system can help players make better decisions and adjust their strategies
-    //accordingly.
-
-    //Overall, these are just a few ideas to help you get started. As you develop your game further,
-    //you can continue to refine and tweak these systems to create a fun and engaging tycoon experience.
-
-
-
-
-
+    public void UpdateUi()
+    {
+        txtGold.text = goldInVault.ToString();
+    }
 }
